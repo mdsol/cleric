50db9d8771ec1d5f0fbb3a97f1f98f98a4a49938
Andrew Smith <asmith@mdsol.com>
2013-05-13 16:09:37 +0100

    Add skeleton 'repo create' command

--- lib/cleric/cli.rb
+++ lib/cleric/cli.rb
@@ -1,6 +1,14 @@
 require 'thor'
 
 module Cleric
+  class Repo < Thor
+    desc "create <name>", "Create the repo <name> and assign a team"
+    def create(name)
+    end
+  end
+
   class CLI < Thor
+    desc 'repo [COMMAND] ...', 'Manage repos'
+    subcommand 'repo', Repo
   end
 end
--- spec/cleric/cli_spec.rb
+++ spec/cleric/cli_spec.rb
@@ -1,9 +1,23 @@
 require 'spec_helper'
 
 describe 'Command line' do
+  let(:cli_command) { '' }
+  let(:cli_help) { `./bin/cleric #{cli_command}` }
+
   context 'when invoked without arguments' do
     it 'shows the available commands' do
-      `./bin/cleric`.should include('cleric help [COMMAND]')
+      cli_help.should include('cleric help [COMMAND]')
+    end
+  end
+
+  it 'has a "repo [COMMAND]" command' do
+    cli_help.should include('cleric repo [COMMAND]')
+  end
+
+  context 'under the "repo" command' do
+    let(:cli_command) { 'repo' }
+    it 'has a "create <name>" command' do
+      cli_help.should include('cleric repo create <name>')
     end
   end
 end
