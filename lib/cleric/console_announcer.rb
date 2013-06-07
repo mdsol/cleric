7bff0c6ef1e2d3a49715ccab9e05b1de55b4e438
Andrew Smith <asmith@mdsol.com>
2013-06-07 16:12:29 +0100

    Refactor string quoting

--- lib/cleric/console_announcer.rb
+++ lib/cleric/console_announcer.rb
@@ -7,32 +7,32 @@ module Cleric
     end
 
     def chatroom_added_to_repo(repo, chatroom)
-      write_success("Repo \"#{repo}\" notifications will be sent to chatroom \"#{chatroom}\"")
+      write_success(%Q[Repo "#{repo}" notifications will be sent to chatroom "#{chatroom}"])
     end
 
     def commits_without_pull_requests(repo, commits)
-      write_warning("Repo \"#{repo}\" has the following commits not covered by pull requests:\n" +
+      write_warning(%Q[Repo "#{repo}" has the following commits not covered by pull requests:\n] +
         commits.join("\n"))
     end
 
     def repo_added_to_team(repo, team)
-      write_success("Repo \"#{repo}\" added to team \"#{team}\"")
+      write_success(%Q[Repo "#{repo}" added to team "#{team}"])
     end
 
     def repo_audit_passed(repo)
-      write_success("Repo \"#{repo}\" passed audit")
+      write_success(%Q[Repo "#{repo}" passed audit])
     end
 
     def repo_created(repo)
-      write_success("Repo \"#{repo}\" created")
+      write_success(%Q[Repo "#{repo}" created])
     end
 
     def user_added_to_team(username, team)
-      write_success("User \"#{username}\" added to team \"#{team}\"")
+      write_success(%Q[User "#{username}" added to team "#{team}"])
     end
 
     def user_removed_from_org(username, email, org)
-      write_success("User \"#{username}\" (#{email}) removed from organization \"#{org}\"")
+      write_success(%Q[User "#{username}" (#{email}) removed from organization "#{org}"])
     end
 
     private
