f51be54713f3f0adc4b3e2aa169cd675af3a0ad2
Geoff Low <glow@mdsol.com>
2013-07-23 17:15:37 +0100

    decorate hipchat messages with username of admin

--- lib/cleric/hipchat_announcer.rb
+++ lib/cleric/hipchat_announcer.rb
@@ -1,33 +1,34 @@
 module Cleric
   class HipChatAnnouncer
-    def initialize(config, listener)
+    def initialize(config, listener, user)
       @config = config
       @listener = listener
+      @user = user
     end
 
     def chatroom_added_to_repo(repo, chatroom)
       @listener.chatroom_added_to_repo(repo, chatroom)
-      send_message(%Q[Repo "#{repo}" notifications will be sent to chatroom "#{chatroom}"])
+      send_message(%Q[Repo "#{repo}" notifications will be sent to chatroom "#{chatroom}", set by "#{@user}"])
     end
 
     def repo_added_to_team(repo, team)
       @listener.repo_added_to_team(repo, team)
-      send_message(%Q[Repo "#{repo}" added to team "#{team}"])
+      send_message(%Q[Repo "#{repo}" added to team "#{team}" by "#{@user}"])
     end
 
     def repo_created(repo)
       @listener.repo_created(repo)
-      send_message(%Q[Repo "#{repo}" created])
+      send_message(%Q[Repo "#{repo}" created by "#{@user}"])
     end
 
     def user_added_to_team(username, team)
       @listener.user_added_to_team(username, team)
-      send_message(%Q[User "#{username}" added to team "#{team}"])
+      send_message(%Q[User "#{username}" added to team "#{team}" by "#{@user}"])
     end
 
     def user_removed_from_org(username, email, org)
       @listener.user_removed_from_org(username, email, org)
-      send_message(%Q[User "#{username}" (#{email}) removed from organization "#{org}"], :red)
+      send_message(%Q[User "#{username}" (#{email}) removed from organization "#{org}" by "#{@user}"], :red)
     end
 
     private
--- spec/cleric/hipchat_announcer_spec.rb
+++ spec/cleric/hipchat_announcer_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 module Cleric
   describe HipChatAnnouncer do
-    subject(:announcer) { HipChatAnnouncer.new(config, listener) }
+    subject(:announcer) { HipChatAnnouncer.new(config, listener, user) }
     let(:config) { mock('Config', hipchat_api_token: 'api_token', hipchat_announcement_room_id: 'room_id') }
     let(:listener) { mock('Listener').as_null_object }
     let(:hipchat) { mock('HipChat').as_null_object }
+    let(:user) {'an_admin'}
 
     before(:each) { HipChat::API.stub(:new) { hipchat } }
 
@@ -27,31 +28,31 @@ module Cleric
     describe '#chatroom_added_to_repo' do
       it_behaves_like :announcing_method, :chatroom_added_to_repo,
         args: ['a_repo', 'a_chatroom'],
-        message: 'Repo "a_repo" notifications will be sent to chatroom "a_chatroom"'
+        message: 'Repo "a_repo" notifications will be sent to chatroom "a_chatroom", set by "an_admin"'
     end
 
     describe '#repo_added_to_team' do
       it_behaves_like :announcing_method, :repo_added_to_team,
         args: ['a_repo', 'a_team'],
-        message: 'Repo "a_repo" added to team "a_team"'
+        message: 'Repo "a_repo" added to team "a_team" by "an_admin"'
     end
 
     describe '#repo_created' do
       it_behaves_like :announcing_method, :repo_created,
         args: ['a_repo'],
-        message: 'Repo "a_repo" created'
+        message: 'Repo "a_repo" created by "an_admin"'
     end
 
     describe '#user_added_to_team' do
       it_behaves_like :announcing_method, :user_added_to_team,
         args: ['a_user', 'a_team'],
-        message: 'User "a_user" added to team "a_team"'
+        message: 'User "a_user" added to team "a_team" by "an_admin"'
     end
 
     describe '#user_removed_from_org' do
       it_behaves_like :announcing_method, :user_removed_from_org,
         args: ['a_user', 'user@example.com', 'an_org'],
-        message: 'User "a_user" (user@example.com) removed from organization "an_org"',
+        message: 'User "a_user" (user@example.com) removed from organization "an_org" by "an_admin"',
         color: 'red'
     end
   end
